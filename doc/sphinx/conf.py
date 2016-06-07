edd504461f5151b083570e63696f63f6a2b11fa8
phonchi <steve2003121@gmail.com>
2016-06-07 22:47:00 +0800

    Update conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -82,7 +82,6 @@ if spelling is not None:
 	
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-nitpicky = True
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -299,7 +298,3 @@ texinfo_documents = [
 intersphinx_mapping = {'https://docs.python.org/3': None}
 
 epub_theme = 'epub'
-
-# Retry requests in the linkcheck builder so that we're resillient against
-# transient network errors.
-linkcheck_retries = 2
