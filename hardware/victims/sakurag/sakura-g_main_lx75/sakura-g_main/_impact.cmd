0b98538af54b0980b4bc7bf7e812bfc4c58497ec
phonchi <steve2003121@gmail.com>
2016-10-02 14:16:46 +0800

    decouple

---
+++ hardware/victims/sakurag/sakura-g_main_lx75/sakura-g_main/_impact.cmd
@@ -0,0 +1,51 @@
+setMode -pff
+setMode -pff
+addConfigDevice  -name "aes_new_gen" -path "D:\github_project\chipwhisperer_scrip\hardware\victims\sakurag\sakura-g_main_lx75\sakura-g_main"
+setSubmode -pffspi
+setAttribute -configdevice -attr multibootBpiType -value ""
+addDesign -version 0 -name "0"
+setMode -pff
+addDeviceChain -index 0
+setMode -pff
+addDeviceChain -index 0
+setAttribute -configdevice -attr compressed -value "FALSE"
+setAttribute -configdevice -attr compressed -value "FALSE"
+setAttribute -configdevice -attr autoSize -value "FALSE"
+setAttribute -configdevice -attr fileFormat -value "mcs"
+setAttribute -configdevice -attr fillValue -value "FF"
+setAttribute -configdevice -attr swapBit -value "FALSE"
+setAttribute -configdevice -attr dir -value "UP"
+setAttribute -configdevice -attr multiboot -value "FALSE"
+setAttribute -configdevice -attr multiboot -value "FALSE"
+setAttribute -configdevice -attr spiSelected -value "TRUE"
+setAttribute -configdevice -attr spiSelected -value "TRUE"
+addPromDevice -p 1 -size 4096 -name 4M
+setMode -pff
+setMode -pff
+setMode -pff
+setMode -pff
+addDeviceChain -index 0
+setMode -pff
+addDeviceChain -index 0
+setSubmode -pffspi
+setMode -pff
+setAttribute -design -attr name -value "0000"
+addDevice -p 1 -file "D:/github_project/chipwhisperer_scrip/hardware/victims/sakurag/sakura-g_main_lx75/sakura-g_main/chip_sakura_g_aes.bit"
+setMode -pff
+setSubmode -pffspi
+generate
+setCurrentDesign -version 0
+setMode -pff
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+deletePromDevice -position 1
+setCurrentDesign -version 0
+deleteDevice -position 1
+deleteDesign -version 0
+setCurrentDesign -version -1
---
+++ hardware/victims/sakurag/sakura-g_main_lx75/sakura-g_main/ise_impact.cmd
@@ -0,0 +1,4 @@
+loadCdf -file "D:/github_project/chipwhisperer_scrip/hardware/victims/sakurag/sakura-g_main_lx75/sakura-g_main/sakura-g_main.ipf"
+setMode -pff
+generate
+exit
